cc026b9a52f5c7763195b564e6ba85bdecd4cf77
Klemens Esterle <klemens.esterle@posteo.de>
2017-04-11 22:41:48 +0200

    improved process noise std deviation to meet project spec

--- src/ukf.cpp
+++ src/ukf.cpp
@@ -42,10 +42,10 @@ UKF::UKF() {
     Xsig_pred_ = MatrixXd(n_x_, n_sig_);
 
     // Process noise standard deviation longitudinal acceleration in m/s^2
-    std_a_ = 2;
+    std_a_ = 4;
 
     // Process noise standard deviation yaw acceleration in rad/s^2
-    std_yawdd_ = 0.1;
+    std_yawdd_ = 0.6;
 
     // Laser measurement noise standard deviation position1 in m
     std_laspx_ = 0.15;
